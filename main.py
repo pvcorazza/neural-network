9730d369fb27b77bf934121afaed26f3dd0410e2
Paulo Victor Corazza <paulovictor@live.com>
2018-11-29 07:42:29 -0200

    Rede neural com função de custo log

--- main.py
+++ main.py
@@ -1,17 +1,57 @@
 import numpy as np
+import sklearn
+import sklearn.datasets
 
 from neural_network import NeuralNetwork
 
-if __name__ == '__main__':
-    entries = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    outputs = np.array([[0], [1], [1], [0]])
 
-    NN = NeuralNetwork(0.24, 2, 3, 1, entries, outputs)
+def load_planar_dataset():
+    np.random.seed(1)
+    m = 400  # number of examples
+    N = int(m / 2)  # number of points per class
+    D = 2  # dimensionality
+    X = np.zeros((m, D))  # data matrix where each row is a single example
+    Y = np.zeros((m, 1), dtype='uint8')  # labels vector (0 for red, 1 for blue)
+    a = 4  # maximum ray of the flower
+
+    for j in range(2):
+        ix = range(N * j, N * (j + 1))
+        t = np.linspace(j * 3.12, (j + 1) * 3.12, N) + np.random.randn(N) * 0.2  # theta
+        r = a * np.sin(4 * t) + np.random.randn(N) * 0.2  # radius
+        X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
+        Y[ix] = j
+
+    X = X.T
+    Y = Y.T
+
+    return X, Y
+
+
+if __name__ == '__main__':
+    entries, outputs = load_planar_dataset()
+    #
+    # entries = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    # outputs = np.array([0, 1, 1, 0])
+
+
+    shape_X = entries.shape
+    shape_Y = outputs.shape
+    num_examples = outputs.shape[1]  # training set size
+    ### END CODE HERE ###
+
+    print(entries)
+    print('The shape of X is: ' + str(shape_X))
+    print(outputs)
+    print('The shape of Y is: ' + str(shape_Y))
+    print('I have m = %d training examples!' % (num_examples))
+
+    num_inputs = shape_X[0]
+    num_hidden = 4
+    num_outputs = shape_Y[0]
+
+    NN = NeuralNetwork(0.24, num_inputs, num_hidden, num_outputs, entries, outputs, num_examples)
 
     NN.train()
-
-    output = NN.forward_propagation(entries)
-    print(output)
 
 
 
--- neural_network.py
+++ neural_network.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class NeuralNetwork:
-    def __init__(self, reg_factor, num_inputs, num_hidden, num_outputs, entries, outputs):
+    def __init__(self, reg_factor, num_inputs, num_hidden, num_outputs, entries, outputs, num_examples):
         # Definição dos parâmetros
         self.reg_factor = reg_factor
         self.num_inputs = num_inputs
@@ -9,20 +9,31 @@ class NeuralNetwork:
         self.num_outputs = num_outputs
         self.entries = entries
         self.outputs = outputs
-        self.epochs = 100000
-        self.learning_rate = 0.6
+        self.num_examples = num_examples
+        self.epochs = 10000
+        self.learning_rate = 1.2
         self.momentum = 1
 
     # Define pesos aleatórios para a rede
     def init_random_weights(self):
+        np.random.seed(2)
         # Camada de entrada para camada oculta
-        self.W1 = np.random.randn(self.num_inputs, self.num_hidden)
+        self.W1 = np.random.randn(self.num_hidden, self.num_inputs) / 100
+        # Bias da primeira camada
+        self.b1 = np.zeros((self.num_hidden, 1))
+        self.b1 = np.array([[1.74481176],
+                            [-0.7612069],
+                            [0.3190391],
+                            [-0.24937038]])
         # Camada oculta para camada de saída
-        self.W2 = np.random.randn(self.num_hidden, self.num_outputs)
+        self.W2 = np.random.randn(self.num_outputs, self.num_hidden) / 100
+        # Bias da segunda camada
+        self.b2 = np.zeros((self.num_outputs, 1))
+        self.b2 = np.array([[-1.3]])
 
     # Função de soma
-    def calculate_sum(self, entries, weights):
-        sum = np.dot(entries, weights)
+    def calculate_sum(self, weights, entries):
+        sum = np.dot(weights, entries)
         return sum
 
     # Função sigmóide
@@ -58,28 +69,59 @@ class NeuralNetwork:
 
     # Função de propagação
     def forward_propagation(self, entries):
-        sum_entries = self.calculate_sum(entries, self.W1)
-        self.activations_hidden_layer = self.calculate_sigmoid(sum_entries)
-        sum_hidden = self.calculate_sum(self.activations_hidden_layer, self.W2)
-        result_output_layer = self.calculate_sigmoid(sum_hidden)
-        return result_output_layer
+        self.sum_entries = self.calculate_sum(self.W1, entries) + self.b1
+        self.activations_hidden_layer = np.tanh(self.sum_entries)
+        self.sum_hidden = self.calculate_sum(self.W2, self.activations_hidden_layer) + self.b2
+        self.activation_output_layer = self.calculate_sigmoid(self.sum_hidden)
+        # print(np.mean(self.sum_entries), np.mean(self.activations_hidden_layer), np.mean(self.sum_hidden), np.mean(self.activation_output_layer))
+
+    def calculate_cost(self):
+        logprobs = np.multiply(np.log(self.activation_output_layer), self.outputs) + np.multiply((1 - self.outputs), np.log(1 - self.activation_output_layer))
+        cost = - np.sum(logprobs) / self.num_examples
+        return cost
+
+    def backward_propagation(self):
+
+        dZ2 = self.activation_output_layer - self.outputs
+        self.derivatives_W2 = (1 / self.num_examples) * np.dot(dZ2, self.activations_hidden_layer.T)
+        self.derivatives_b2 = (1 / self.num_examples) * np.sum(dZ2, axis=1, keepdims=True)
+        dZ1 = np.multiply(np.dot(self.W2.T, dZ2), 1 - np.power(self.activations_hidden_layer, 2))
+        self.derivatives_W1 = (1 / self.num_examples) * np.dot(dZ1, self.entries.T)
+        self.derivatives_b1 = (1 / self.num_examples) * np.sum(dZ1, axis=1, keepdims=True)
+
+    def update_parameters(self):
+
+        self.W1 -= self.learning_rate * self.derivatives_W1
+        self.b1 -= self.learning_rate * self.derivatives_b1
+        self.W2 -= self.learning_rate * self.derivatives_W2
+        self.b2 -= self.learning_rate * self.derivatives_b2
+
 
     def train(self):
         self.init_random_weights()
-        for j in range (self.epochs):
-            output = self.forward_propagation(self.entries)
-            errors = self.calculate_output_error(self.outputs, output)
-            self.absolute_mean = np.mean(np.abs(errors))
-            output_derivative = self.calculate_derivative_sigmoid(output)
-            output_delta = self.calculate_output_delta(errors, output_derivative)
-            hidden_derivative = self.calculate_derivative_sigmoid(self.activations_hidden_layer)
-            hidden_delta = self.calculate_hidden_delta(hidden_derivative, self.W2, output_delta)
-            self.W2 = self.calculate_new_weights(self.W2, self.activations_hidden_layer, output_delta)
-            self.W1 = self.calculate_new_weights(self.W1, self.entries, hidden_delta)
+        for i in range (self.epochs):
+            self.forward_propagation(self.entries)
+            cost = self.calculate_cost()
+            self.backward_propagation()
+            self.update_parameters()
 
-            # print(self.W1)
-            # print("------")
-            # print(self.W2)
+            # Print the cost every 1000 iterations
+            if i % 1000 == 0:
+                print("Cost after iteration %i: %f" % (i, cost))
+
+
+        # errors = self.calculate_output_error(self.outputs, output)
+        # self.absolute_mean = np.mean(np.abs(errors))
+        # output_derivative = self.calculate_derivative_sigmoid(output)
+        # output_delta = self.calculate_output_delta(errors, output_derivative)
+        # hidden_derivative = self.calculate_derivative_sigmoid(self.activations_hidden_layer)
+        # hidden_delta = self.calculate_hidden_delta(hidden_derivative, self.W2, output_delta)
+        # self.W2 = self.calculate_new_weights(self.W2, self.activations_hidden_layer, output_delta)
+        # self.W1 = self.calculate_new_weights(self.W1, self.entries, hidden_delta)
+
+        # print(self.W1)
+        # print("------")
+        # print(self.W2)
 
 
 
